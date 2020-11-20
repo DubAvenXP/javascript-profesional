85d8135e3a3db85323441ae7a3e85408189e82ce
Alejandro Dubon <alejandrodubon88@gmail.com>
2020-11-19 22:50:06 -0600

    cambios al 19-11-2020

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,18 +1,53 @@
 // Esta funcion MediaPlayer funciona como una clase, pero es un prototype
-function MediaPlayer(confing) {
+function MediaPlayer(config) {
   // this.media tendra el elemento de video almacenado
   // .element es un atributo del objeto config
-  this.media = confing.element;
-  //this.plugins = confing.plugins || [];
+  this.media = config.element;
+  this.plugins = config.plugins || [];
+  this._initPlugins();
 }
+
+
+
+MediaPlayer.prototype._initPlugins = function _initPlugins() {
+  const player = {
+    play: () => this.play(),
+    playAndPause: () => this.playAndPause,
+    media: this.media,
+    set muted(value) {
+      this.media.muted = value;
+    } 
+  }
+  
+  this.plugins.forEach((plugin) => {
+    plugin.run(player);
+  });
+
+
+
+};
+MediaPlayer.prototype.mute = function mute(value){
+  this.media.muted = value;
+};
+MediaPlayer.prototype.play = function play(){
+  this.media.play();
+};
 
 // // La clase MediaPlayer tiene un metodo llamado playAndPause
 
 MediaPlayer.prototype.playAndPause = function playAndPause() {
+  
   if (this.media.paused) {
     this.media.play();
   } else {
     this.media.pause();
+  }
+};
+MediaPlayer.prototype.muteAndUnmute = function muteAndUnmute() {
+  if (this.media.muted) {
+    this.media.muted = false;
+  } else {
+    this.media.muted = true;
   }
 };
 
--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,17 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const button = document.querySelector('button');
+const playButton = document.getElementById('play');
+const muteButton = document.getElementById('mute');
 
 // player es una instancia de la clase MediaPlayer
 // por medio de destructuracion de objetos el atributo element recibira como valor video
-const player = new MediaPlayer({ element: video });
+const player = new MediaPlayer({ element: video, plugins: [
+    //new AutoPlay()
+] });
 
 // cuando el boton es presionado se ejecuta la siguiente arrow function
-// button.onclick = () => video.paused ? player.play() : player.pause;
-button.onclick = () => player.playAndPause();
+// playButton.onclick = () => video.paused ? player.play() : player.pause;
+playButton.onclick = () => player.playAndPause();
+muteButton.onclick = () => player.muteAndUnmute();
